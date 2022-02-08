fde19ee22f883ba35627a9b474baa2077cf77036
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-02-08 16:38:04 +0900

    js_basic_theory

---
+++ syntax/template.js
@@ -0,0 +1,10 @@
+let name = 'kamja';
+let letter = `From Wikipedia, the free encyclopedia
+            Jump to navigationJump to search
+            "Ipsum" redirects here. For the car, 
+            ${name} ${1+1000}
+             see Toyota Ipsum.
+            An example of the Lorem ipsum placeholder text on a green and white webpage.
+            Using Lorem ipsum to focus attention on graphic elements in a webpage design proposal
+            In publishing and graphic design, `;
+console.log(letter);
